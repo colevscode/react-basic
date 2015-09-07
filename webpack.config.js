8b2901ff3fbd3ca51686177f6393d6c9fabbbcf3
Cole Krumbholz <colevscode@Coles-MacBook-Air.local>
2015-09-06 23:19:40 -0400

    standardizing a bit

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
+var path = require('path');
+
 module.exports = {
-  entry: ["./js/main.js"],
+  entry: ["./src/index.js"],
   output: {
     path: './build', // This is where images AND js will go
     filename: 'bundle.js'
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader' },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [path.resolve(__dirname, "src")]
+      },
       { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' }, // use ! to chain loaders
       { test: /\.css$/, loader: 'style-loader!css-loader' },
       { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' } // inline base64 URLs for <=8k images, direct URLs for the rest
